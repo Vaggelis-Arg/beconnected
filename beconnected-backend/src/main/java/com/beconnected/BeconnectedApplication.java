806794d2ea40b82a3a7a3eaf65a1be0acda18433
Evangelos Argyropoulos <vag.argyropoulos@gmail.com>
2024-07-15 12:33:41 +0300

    spring initializr commit

---
+++ beconnected-backend/src/main/java/com/beconnected/BeconnectedApplication.java
@@ -0,0 +1,13 @@
+package com.beconnected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeconnectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeconnectedApplication.class, args);
+	}
+
+}
---
+++ beconnected-backend/src/test/java/com/beconnected/BeconnectedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beconnected;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeconnectedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
