96534fa9bee3e24e898243d1b6662ef93e272c81
Evangelos Argyropoulos <vag.argyropoulos@gmail.com>
2024-08-03 07:06:25 +0300

    added styling for profile page

--- beconnected-frontend/src/components/Profile/Profile.jsx
+++ beconnected-frontend/src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getUserInfoByUsername } from "../../api/Api";
+import './profile.css'
 
 const Profile = () => {
     const { username } = useParams();
