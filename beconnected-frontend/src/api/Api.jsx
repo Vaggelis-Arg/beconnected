1ec2ba3c0117968c5b148025a4593151fabc2875
dboutzounis <dboutzounis2004@gmail.com>
2024-08-21 01:41:22 +0300

    fixed ssl

--- beconnected-frontend/src/api/Api.jsx
+++ beconnected-frontend/src/api/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:8080/api';
+export const API_URL = '/api';
 
 export const login = async (usernameOrEmail, password) => {
     try {
